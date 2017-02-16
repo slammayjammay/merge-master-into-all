3b125784adcea1b97110cfbc1f916532ff0bd803
Scott McKenzie <slammayjammay@gmail.com>
2017-02-16 12:56:55 -0800

    Merge master into all branches

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+/**
+ * NOTE: This is heavily based on `git branch` output.
+ */
+
 const execSync = require('child_process').execSync
 
-let branches = execSync('git branch')
-console.log(branches)
+let output = execSync('git branch').toString('utf8')
+
+// get array of branches
+let branches = output.split('\n').filter(branch => branch !== '')
+
+// trim whitespace
+branches = branches.map(branch => branch.trim())
+
+// filter out the leading '* ' on the current branch
+let currentBranch = branches.find(branch => branch[0] === '*')
+let currentBranchIdx = branches.indexOf(currentBranch)
+branches[currentBranchIdx] = currentBranch.slice(2)
+
+// filter out master branch
+branches.splice(branches.indexOf('master'), 1)
+
+// merge master into each branch
+branches.forEach(branch => {
+	execSync(`git checkout ${branch}`)
+	execSync(`git merge master`)
+})
