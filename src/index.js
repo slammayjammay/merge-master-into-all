8b9a778a3c7964090c3b5865bcc2e9340f0439d9
slammayjammay <slammayjammay@gmail.com>
2017-02-16 13:35:40 -0800

    Fix

--- src/index.js
+++ src/index.js
@@ -17,12 +17,14 @@ branches = branches.map(branch => branch.trim())
 
 // filter out the leading '* ' on the current branch
 let currentBranch = branches.find(branch => branch[0] === '*')
-currentBranch = currentBranch.slice(2)
 let currentBranchIdx = branches.indexOf(currentBranch)
+currentBranch = currentBranch.slice(2)
 branches[currentBranchIdx] = currentBranch
+
 
 // filter out master branch
 branches.splice(branches.indexOf('master'), 1)
+
 
 // merge master into each branch
 branches.forEach(branch => {
